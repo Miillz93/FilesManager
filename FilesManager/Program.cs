836f91a2e5703d42e55f30fef883b518e968c55a
Romaric <romaric.miaka.sio@gmail.com>
2023-06-14 22:51:58 +0200

    save

--- FilesManager/Program.cs
+++ FilesManager/Program.cs
@@ -1,53 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-System.Console.WriteLine("Start Program.................................");
+﻿using Shared;
+
+
+Console.WriteLine("Start Program.................................");
 
 
 bool continued = true;
-int selector = 0        ;
 
-// while (continued)
-// {
+while (continued)
+{
+            // Console.WriteLine("Select one of the following");
+            // Console.WriteLine("1 - Moves files");
+            // Console.WriteLine("0 - Exit from Console App");
+            // string? strSelector = Console.ReadLine();
 
-//     Console.WriteLine("Select one of the following");
-//     Console.WriteLine("1 - Moves files");
-//     Console.WriteLine("0 - Exit from Console App");
-//     string strSelector = Console.ReadLine();
+            // bool success = int.TryParse(strSelector, out int selector);
+            // if(!success) selector = 2;
 
-//     try{
-//         selector = int.Parse(strSelector);
-//     }catch(FormatException){
-//         Console.WriteLine("Bad format");
-//     }
 
-//     switch(selector){
-//         case 0:
-//             continued = true;
-//             break;
-//         case 1: 
-//             System.Console.WriteLine("Enter file Path");
-//             string filePath =  Console.ReadLine();
-//             System.Console.WriteLine("file path : {0}", filePath);
-//             System.Console.WriteLine("Enter Video Path");
-//             string videoDirectory = Console.ReadLine();
-//             System.Console.WriteLine("Directory path : {0}", videoDirectory);
+            //     switch(selector){
+            //         case 0:
+            //             continued = true;
+            //             break;
+            //         case 1: 
+            //             System.Console.WriteLine("Enter file Path");
+            //             string filePath =  Console.ReadLine();
+            //             System.Console.WriteLine("file path : {0}", filePath);
+            //             System.Console.WriteLine("Enter Video Path");
+            //             string videoDirectory = Console.ReadLine();
+            //             System.Console.WriteLine("Directory path : {0}", videoDirectory);
 
-//             var Explorer = new Explorer(filePath, videoDirectory);
-//             Explorer.MoveFileFromOneDirToAnother();
-//             break;
-//         default:
-//             Console.WriteLine("Invalid number selected {0}", selector);
-//             break;
-//     }   
-// }
-        string filePath = "";
-        string videoDirectory = "";
+            //     break;
+            // default:
+            //     Console.WriteLine("Invalid number selected {0}", selector);
+            //     break;
+            // }   
+}
 
-        var Explorer = new Explorer(filePath, videoDirectory);
-        Explorer.MoveFileFromOneDirToAnotherAsync();
-
+Console.WriteLine("End  Program................................");
 Console.ReadKey();
-
-
-
-
-
