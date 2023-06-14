727d65e943428676a587027c6bd1cb5afaf7cb0d
Romaric <romaric.miaka.sio@gmail.com>
2023-06-14 15:02:31 +0200

    add data manager

---
+++ FilesManager/FileData.cs
@@ -0,0 +1,17 @@
+﻿namespace filesmanager;
+
+public class FileData
+{
+    public string FileSource { get; set;}
+    public string FolderSource { get; set;}
+    public string FileDestination { get; set; }
+    public string FolderDestination { get; set; }
+    public string LogFileSource { get; set; }
+    public string logFolderSource { get; set; }
+    public string LogFileDestination { get; set; }
+    public string logFolderDestination { get; set; }
+    public string[]? NameCombined { get; set; }
+    public string[]? FolderCombinedSource {get; set;}
+
+
+}
