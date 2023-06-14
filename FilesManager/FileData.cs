8afa58fe3cfd0b950c7de0950cb04023c774c34a
Romaric <romaric.miaka.sio@gmail.com>
2023-06-14 16:23:29 +0200

    nullable

--- FilesManager/FileData.cs
+++ FilesManager/FileData.cs
@@ -2,14 +2,14 @@
 
 public class FileData
 {
-    public string FileSource { get; set;}
-    public string FolderSource { get; set;}
-    public string FileDestination { get; set; }
-    public string FolderDestination { get; set; }
-    public string LogFileSource { get; set; }
-    public string logFolderSource { get; set; }
-    public string LogFileDestination { get; set; }
-    public string logFolderDestination { get; set; }
+    public string? FileSource { get; set;}
+    public string? FolderSource { get; set;}
+    public string? FileDestination { get; set; }
+    public string? FolderDestination { get; set; }
+    public string? LogFileSource { get; set; }
+    public string? LogFolderSource { get; set; }
+    public string? LogFileDestination { get; set; }
+    public string? LogFolderDestination { get; set; }
     public string[]? NameCombined { get; set; }
     public string[]? FolderCombinedSource {get; set;}
 
