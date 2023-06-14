95ea5ea7c24dacfa8524fd9de74d178db76ec439
miillz <romaric.miaka.sio@gmail.com>
2023-06-14 19:20:43 +0200

    program sample

--- FilesManager/Program.cs
+++ FilesManager/Program.cs
@@ -5,36 +5,46 @@ System.Console.WriteLine("Start Program.................................");
 bool continued = true;
 int selector = 0        ;
 
-while (continued)
-{
+// while (continued)
+// {
 
-    Console.WriteLine("Select one of the following");
-    Console.WriteLine("1 - Moves files");
-    Console.WriteLine("0 - Exit from Console App");
-    string strSelector = Console.ReadLine();
+//     Console.WriteLine("Select one of the following");
+//     Console.WriteLine("1 - Moves files");
+//     Console.WriteLine("0 - Exit from Console App");
+//     string strSelector = Console.ReadLine();
 
-    try{
-        selector = int.Parse(strSelector);
-    }catch(FormatException){
-        Console.WriteLine("Bad format");
-    }
+//     try{
+//         selector = int.Parse(strSelector);
+//     }catch(FormatException){
+//         Console.WriteLine("Bad format");
+//     }
 
-    switch(selector){
-        case 0:
-            continued = true;
-            break;
-        case 1: 
-            System.Console.WriteLine("case 1 begin");
+//     switch(selector){
+//         case 0:
+//             continued = true;
+//             break;
+//         case 1: 
+//             System.Console.WriteLine("Enter file Path");
+//             string filePath =  Console.ReadLine();
+//             System.Console.WriteLine("file path : {0}", filePath);
+//             System.Console.WriteLine("Enter Video Path");
+//             string videoDirectory = Console.ReadLine();
+//             System.Console.WriteLine("Directory path : {0}", videoDirectory);
 
-            break;
-        default:
-            Console.WriteLine("Invalid number selected {0}", selector);
-            break;
-    }   
-}
+//             var Explorer = new Explorer(filePath, videoDirectory);
+//             Explorer.MoveFileFromOneDirToAnother();
+//             break;
+//         default:
+//             Console.WriteLine("Invalid number selected {0}", selector);
+//             break;
+//     }   
+// }
+        string filePath = "";
+        string videoDirectory = "";
 
+        var Explorer = new Explorer(filePath, videoDirectory);
+        Explorer.MoveFileFromOneDirToAnotherAsync();
 
-Console.WriteLine("End  Program.................................");
 Console.ReadKey();
 
 
