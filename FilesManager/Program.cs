ac5ce0f8970b122eeb55402ae5245e9d82d4fa75
Romaric <romaric.miaka.sio@gmail.com>
2023-06-18 02:52:49 +0200

    remove snip code

--- FilesManager/Program.cs
+++ FilesManager/Program.cs
@@ -3,38 +3,9 @@
 
 Console.WriteLine("Start Program.................................");
 
+SampleData sampleData= new();
 
-bool continued = true;
-
-while (continued)
-{
-            // Console.WriteLine("Select one of the following");
-            // Console.WriteLine("1 - Moves files");
-            // Console.WriteLine("0 - Exit from Console App");
-            // string? strSelector = Console.ReadLine();
-
-            // bool success = int.TryParse(strSelector, out int selector);
-            // if(!success) selector = 2;
-
-
-            //     switch(selector){
-            //         case 0:
-            //             continued = true;
-            //             break;
-            //         case 1: 
-            //             System.Console.WriteLine("Enter file Path");
-            //             string filePath =  Console.ReadLine();
-            //             System.Console.WriteLine("file path : {0}", filePath);
-            //             System.Console.WriteLine("Enter Video Path");
-            //             string videoDirectory = Console.ReadLine();
-            //             System.Console.WriteLine("Directory path : {0}", videoDirectory);
-
-            //     break;
-            // default:
-            //     Console.WriteLine("Invalid number selected {0}", selector);
-            //     break;
-            // }   
-}
+Menu.MainMenu(sampleData);
 
 Console.WriteLine("End  Program................................");
 Console.ReadKey();
