d67846f8c21e78ee9a0dccf33dcd37ef8a851cb2
Romaric <romaric.miaka.sio@gmail.com>
2023-06-18 02:54:13 +0200

    data model replacement

---
+++ Shared/SampleData.cs
@@ -0,0 +1,20 @@
+﻿namespace Shared;
+
+public class SampleData
+{
+    public string? EmbeedPath {get; set;}
+    public string? FilePath {get; set;}
+    public string? PathSource {get; set;}
+    public string? FileDestination {get; set;}
+    public string? PathDestination {get; set;}
+    public string? VideoPath {get; set;}
+    public string? LogFileSource {get; set;}
+    public string? LogPathSource {get; set;}
+    public string? LogFileDestination  {get; set;}
+    public string? LogPathDestination {get; set;}
+    public string? PlaylistName {get; set;}
+    public string[]? PathCombinedName {get; set;}
+    public string[]? ExcludeFolderName {get; set;}
+    public string []? PathCombinedSource {get; set;}
+
+}
