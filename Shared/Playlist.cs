3f19d2db9b63d526f0afc032f29d26c9ba6cd607
Romaric <romaric.miaka.sio@gmail.com>
2023-06-19 16:32:24 +0200

    note

--- Shared/Playlist.cs
+++ Shared/Playlist.cs
@@ -2,9 +2,10 @@
 
 public class Playlist
 {
-    public string? Name {get; set;}
-    public string? UniquePath {get; set;}
-    public string? TrackFileUniquePath {get; set;}
+    public string? Name {get; set;} // Playlist Name
+    public string? Base {get; set;} // Playlist endpoint
+    public string? UniquePath {get; set;} //Playlist subfolder combine with base and Name Playlist
+    public string? TrackFileUniquePath {get; set;} // Path of unique folder -> Feb
     public string? TrackFileDuplicatePath {get;set;}
     public List<string>? ExcludeFolderName {get; set;}
     public List<string>? IncludeOnly {get; set;} 
