d9564b878ff5dbbad691e632399cf70660e696cc
Romaric <romaric.miaka.sio@gmail.com>
2023-06-22 11:46:36 +0200

    up model

--- Shared/SampleData.cs
+++ Shared/SampleData.cs
@@ -3,11 +3,13 @@
 public class SampleData
 {
     public string? EmbeedPath {get; set;}
+    public string? EmbeedDestination {get; set;}
     public string? FilePath {get; set;}
     public string? PathSource {get; set;}
     public string? FileDestination {get; set;}
     public string? PathDestination {get; set;}
-    public string? VideoPath {get; set;}
+    public string[]? ExcludeItem {get;set;}
+    public string[]? VideoPath {get; set;}
     public string? Action {get; set;}
     public string? LogFileName {get; set;}
     public string? LogPath {get; set;}
