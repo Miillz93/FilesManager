78987c69aa91121de09be2d49736c8d68f7d53fd
Romaric <romaric.miaka.sio@gmail.com>
2023-07-01 13:58:31 +0200

    refactor

--- Shared/Playlist.cs
+++ Shared/Playlist.cs
@@ -2,14 +2,14 @@
 
 public class Playlist
 {
-    public string? Name {get; set;} // Playlist Name
-    public string? Base {get; set;} // Playlist endpoint
-    public string? UniquePath {get; set;} //Playlist subfolder combine with base and Name Playlist
+    public string? PlaylistName {get; set;} 
+    public string? BasePath {get; set;} // Playlist endpoint
+    public string? UniquePathSource {get; set;} //Playlist subfolder combine with base and Name Playlist
     public string? TrackFileUniquePath {get; set;} // Path of unique folder -> Feb
     public string? TrackFileDuplicatePath {get;set;}
     public List<string>? ExcludeFolderName {get; set;}
     public List<string>? IncludeOnly {get; set;} 
-    public List<string>? MixPathCombinedName {get; set;}
-    public List<string>? PathCombinedSource {get; set;}
+    public List<string>? PlaylistMixName {get; set;}
+    public List<string>? PathMixSource {get; set;}
     public int? MaxCount {get; set;}
 }
