e5acdf9d79436edb6be9c4471132afa24c61ebd9
Romaric <romaric.miaka.sio@gmail.com>
2023-07-03 11:02:37 +0200

    up playlist model

--- Shared/Playlist.cs
+++ Shared/Playlist.cs
@@ -3,13 +3,14 @@
 public class Playlist
 {
     public string? PlaylistName {get; set;} 
-    public string? BasePath {get; set;} // Playlist endpoint
+    public string[]? BasePath {get; set;} // Playlist endpoint
     public string? UniquePathSource {get; set;} //Playlist subfolder combine with base and Name Playlist
     public string? TrackFileUniquePath {get; set;} // Path of unique folder -> Feb
     public string? TrackFileDuplicatePath {get;set;}
-    public List<string>? ExcludeFolderName {get; set;}
-    public List<string>? IncludeOnly {get; set;} 
-    public List<string>? PlaylistMixName {get; set;}
-    public List<string>? PathMixSource {get; set;}
+    public string? PlaylistPathDestination {get; set;}
+    public string[]? ExcludeFolderName {get; set;}
+    public string[]? IncludeOnly {get; set;} 
+    public string[]? PlaylistMixName {get; set;}
+    public string[]? PathMixSource {get; set;}
     public int? MaxCount {get; set;}
 }
