328a3db7c7c5261c12b5d826aa6e64f608e1b574
Romaric <romaric.miaka.sio@gmail.com>
2023-07-03 16:12:01 +0200

    load app

---
+++ FilesManager/application/App.cs
@@ -0,0 +1,25 @@
+﻿using Manager;
+
+namespace Application;
+
+public static class App
+{
+    public static async Task LoadApplicationAsync(){
+        bool check = true;
+
+       //while (check) {
+        var data = await Helpers.ReloadJson();
+        Console.WriteLine(Environment.NewLine);
+
+        await FileManager.CopyOrMoveFileFromSourceFileAsync(data.PathDestination, data.FileMultiPath, 
+                                    data.VideoPath, data.SameSymbol, data.Action); 
+
+        // var index = await Platform.LoadPlatformAsync();
+        // if(index == -1) check = false;
+
+        Console.WriteLine(Environment.NewLine);
+    //    }
+
+    }
+
+}
