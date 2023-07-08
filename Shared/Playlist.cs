51f42113ff742ff1568b91e2942ac364abe03285
Romaric <romaric.miaka.sio@gmail.com>
2023-07-08 20:24:39 +0200

    update playlist model

--- Shared/Playlist.cs
+++ Shared/Playlist.cs
@@ -5,14 +5,14 @@ public class Playlist
     public string? PlaylistName {get; set;} 
     public string[]? BasePath {get; set;} // Playlist endpoint
     public string? UniquePathSource {get; set;} //Playlist subfolder combine with base and Name Playlist
-    public string? TrackFileUniquePath {get; set;} // Path of unique folder -> Feb
-    public string? TrackFileDuplicatePath {get;set;}
+    public string? TrackPlaylist {get; set;} // Path of unique folder -> Feb
+    public string? TrackTracklist {get;set;}
     public string? PlaylistPathDestination {get; set;}
     public string? TracklistPathDestination {get; set;}
     public string[]? CopyType {get; set;}
     public string[]? ExcludeFolderName {get; set;}
     public string[]? IncludeOnly {get; set;} 
-    public string[]? PlaylistMixName {get; set;}
-    public string[]? PathMixSource {get; set;}
-    public int? MaxCount {get; set;}
+    public string? TracklistName {get; set;}
+    // public string[]? PathMixSource {get; set;}
+    public int MaxCount {get; set;}
 }
