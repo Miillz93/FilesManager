08f484d6fed8f4c5d6d0b99d93059d541f2574bb
Romaric <romaric.miaka.sio@gmail.com>
2023-07-10 23:31:36 +0200

    add submenu basic palylist

--- FilesManager/application/Menu.cs
+++ FilesManager/application/Menu.cs
@@ -266,9 +266,8 @@ public static class Menu{
                     Environment.Exit(0);
                     break;
                 case 1:
-
-                    await PlaylistManager.GenerateGenericPlaylist(data, "one");
-                    // await PlaylistManager.GeneratePlaylist(data, "multi");
+                    await SubMenuLevelTwoPlayListBasiType(data, index , platformId);
+                    
 
                     break;
                 case 2: 
@@ -291,6 +290,57 @@ public static class Menu{
 
         return index;
     }
+
+    public static async Task<int> SubMenuLevelTwoPlayListBasiType (SampleData data, int index, int platformId){
+        bool continued = true;
+
+        while (continued)
+        {
+            Console.Clear();
+            Helpers.GetWelcomePage(platformId) ; 
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("BASIC PLAYLIST \n---------------------");
+            Console.WriteLine("0 ► Exit from Console ❌");
+            Console.WriteLine("1 ► UNIQUE🎵");
+            Console.WriteLine("2 ► MIX 🎶");
+            Console.WriteLine("3 ► Reload 🟠");
+            Console.WriteLine("4 ► Back ⏪");
+
+            string? strSelector = Console.ReadLine();
+
+            bool success = int.TryParse(strSelector, out int selector);
+            if(!success) selector = -1;
+
+            await Task.Delay(100);
+            data = await Helpers.ReloadJson();
+
+            switch(selector){
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                case 1:
+                    await PlaylistManager.GenerateGenericPlaylist(data, "one");
+                    break;
+                case 2: 
+                    await PlaylistManager.GenerateGenericPlaylist(data, "multi");
+  
+                    break;                
+                case 3: 
+                    continued = true;
+                    break;
+                case 4: 
+                    continued = false;
+                    break;
+
+                default:
+                    Console.WriteLine("----------------------- Invalid number selected {0}", selector);
+                    break;
+            }   
+        }
+
+        return index;
+    }
+
 
 
     public static async Task<int> SubMenuLevelTwoTrackList (SampleData data, int index, int platformId){
