81416ee4c9468795f923338b0261dac78767ae80
Romaric <romaric.miaka.sio@gmail.com>
2023-07-11 20:05:01 +0200

    export tracklist

--- FilesManager/manager/Tracklist.cs
+++ FilesManager/manager/Tracklist.cs
@@ -10,13 +10,19 @@ public static class TracklistManager
     /// </summary>
     /// <param name="data"></param>
     /// <returns></returns>
-    public static async Task GenerateGenericTracklist(SampleData data) {
-        var tracking = await FileManager.ReadContentWithSpecificInfos(data.Playlist?.TrackTracklist ??"", 2);
+    public static async Task GenerateGenericTracklist(SampleData data)
+    {
+        var tracking = await FileManager.ReadContentWithSpecificInfos(data.Playlist?.TrackTracklist ?? "", 2);
         var playlist = await CreateTracklistWithoutDuplicateDatas(data ?? new(), tracking, "multi", data!.Playlist!.MaxCount);
+        await ExportTracklist(data, playlist);
+    }
 
-        if(playlist.Count != 0){
+    private static async Task ExportTracklist(SampleData data, List<string> playlist)
+    {
+        if (playlist.Count != 0)
+        {
             var newPath = await FileManager.CreateDocument(data?.Playlist?.TrackTracklist ?? "", data?.Playlist?.PlaylistName ?? "");
-            string path = await FileManager.GetDocument(data?.Playlist?.TrackTracklist ??"", 2);
+            var path = await FileManager.GetDocument(data?.Playlist?.TrackTracklist ?? "", 2);
             var pathRoot = Path.GetDirectoryName(newPath);
 
             Thread.Sleep(1000);
@@ -29,14 +35,11 @@ public static class TracklistManager
                 Thread.Sleep(300);
                 await FileManager.ExportPathToDocumentAsync(newPath, item);
                 Thread.Sleep(300);
-                var destinationPath = Path.Combine(pathRoot ??"", Path.GetFileName(item));
+                var destinationPath = Path.Combine(pathRoot ?? "", Path.GetFileName(item));
                 await FileManager.CopyAsync(item, destinationPath);
             }
-
         }
         else await FileManager.CopyAsync(playlist, "");
-        
-
     }
 
     public static async Task<List<string>> CreateTracklistWithoutDuplicateDatas(SampleData data, List<string> fileToRead, string type, int counter)
@@ -52,7 +55,7 @@ public static class TracklistManager
 
     public static async Task<List<string>> CreateTracklistWithoutFilter(SampleData data, List<string> fileToRead, string type, int counter)
     {
-        var playlistLoader = await PlaylistManager.LoadPlaylistDataWhitoutFilter(data, type);
+        var playlistLoader = await PlaylistManager.LoadPlaylistDataWithoutFilter(data, type);
         
         var noDuplicatePlaylist = await PlaylistManager.IsNotDuplicated(playlistLoader ?? new(), fileToRead.ToArray());
 
